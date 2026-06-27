68e836c4f3633c19bca3b89a8572ca0086ef12db
RB3572 <86612265+RB3572@users.noreply.github.com>
2026-06-27 02:55:23 -0400

    Mirror loading of professional editor stylesheet

--- tools/static_templates/app.js
+++ tools/static_templates/app.js
@@ -1,3 +1,7 @@
+const editorStyle = document.createElement("link");
+editorStyle.rel = "stylesheet";
+editorStyle.href = "editor.css";
+document.head.appendChild(editorStyle);
 const headerShortcut = document.getElementById("diy-open");
 if (headerShortcut) headerShortcut.parentNode?.removeChild(headerShortcut);
 import("./app-main.js");
